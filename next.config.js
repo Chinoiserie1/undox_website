81ddb4e9338b015c818c296b0c803aaaa6595692
chixx <lucottejeremie@gmail.com>
2023-10-21 18:47:24 +0200

    fix env variable in next config part 3

--- next.config.js
+++ next.config.js
@@ -5,7 +5,9 @@ const { parsed: myEnv } = require("dotenv").config();
 const nextConfig = {
   webpack(config) {
     if (!myEnv) {
-      throw new Error("Failed to load environment variables from .env file");
+      // throw new Error("Failed to load environment variables from .env file");
+      config.plugins.push(new webpack.EnvironmentPlugin("undefined"));
+      return config;
     }
 
     config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
