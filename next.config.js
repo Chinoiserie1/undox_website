896ed419d4cbe4dd97b91fecdebd4141eef52518
chixx <lucottejeremie@gmail.com>
2023-10-21 18:37:07 +0200

    fix env variable in next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const webpack = require("webpack");
 const { parsed: myEnv } = require("dotenv").config();
+const Dotenv = require("dotenv-webpack");
 
 const nextConfig = {
   webpack(config) {
     config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
     return config;
+  },
+  env: {
+    PROJECT_ID: process.env.PROJECT_ID || "default_value",
   },
 };
 
