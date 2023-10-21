ab0e9263fcb1fb13c76c4e28b863fd8cc4c68a66
chixx <lucottejeremie@gmail.com>
2023-10-21 18:53:45 +0200

    fix env variable in next config finish

--- next.config.js
+++ next.config.js
@@ -1,26 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const webpack = require("webpack");
-// const { parsed: myEnv } = require("dotenv").config();
-
-// const nextConfig = {
-//   webpack(config) {
-//     if (!myEnv) {
-//       config.plugins.push(
-//         new webpack.EnvironmentPlugin(
-//           new Dotenv({
-//             path: "./.env", // Path to .env file (this is the default)
-//             safe: true, // load .env.example (defaults to "false" which does not use dotenv-safe)
-//           })
-//         )
-//       );
-//       return config;
-//     }
-
-//     config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
-//     return config;
-//   },
-// };
-
 // module.exports = nextConfig;
 /** @type {import('next').NextConfig} */
 const webpack = require("webpack");
