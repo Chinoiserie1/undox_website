eeea5548650a93cefe0737679113de53312c2147
chixx <lucottejeremie@gmail.com>
2023-10-22 19:42:57 +0200

    add vercel postgres for shipping informations

---
+++ src/app/api/storeShippingInfo.js
@@ -0,0 +1,34 @@
+import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
+
+export async function CreateTable(request) {
+  try {
+    const result = await sql`CREATE TABLE ShippingInfo (
+        ETHAddress varchar(255),
+        fullName varchar(255),
+        email varchar(255),
+        address VARCHAR(255) NOT NULL,
+        city VARCHAR(255) NOT NULL,
+        postal VARCHAR(20) NOT NULL,
+        country VARCHAR(255) NOT NULL
+      );
+    `;
+    return NextResponse.json({ result }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
+
+export async function handler(req) {
+  const { ETHAddress, fullName, email, address, city, postal, country } = req;
+  console.log(req);
+  try {
+    const result =
+      await sql`INSERT INTO ShippingInfo (Ethaddress, Fullname, Email, Address, City, Postal, Country) VALUES
+        (${ETHAddress}, ${fullName}, ${email}, ${address}, ${city}, ${postal}, ${country});`;
+    return NextResponse.json({ result }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
--- src/components/mint/mint.jsx
+++ src/components/mint/mint.jsx
@@ -3,12 +3,45 @@ import { useState } from "react";
 import { useAccount } from "wagmi";
 import ConnectWallet from "./connectWallet";
 import DisplayWallet from "./displayWallet";
+import { handler } from "@/app/api/storeShippingInfo";
 
 const Mint = () => {
   const currentStatus = 0;
   const status = ["Allowlist", "Whitelist", "Public"];
   const { address } = useAccount();
   const connected = address ? true : false;
+
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    address: "",
+    city: "",
+    postal: "",
+    country: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const request = { ...formData, ETHAddress: address };
+
+    try {
+      const response = await handler(request);
+
+      if (response.ok) {
+        console.log("Data stored successfully!");
+      } else {
+        console.error("Failed to store data");
+      }
+    } catch (error) {
+      console.error("Error storing data:", error);
+    }
+  };
 
   return (
     <div className="pt-10">
@@ -43,72 +76,95 @@ const Mint = () => {
             enthusiasts worldwide.
           </p>
         </div>
-        <div className="px-4 py-5 text-base sm:p-6">
-          <div className="flex flex-wrap -mx-2">
-            <div className="flex flex-col flex-wrap w-full -mx-2 sm:flex-row">
-              <div className="w-full px-2 mb-4 sm:w-1/2 sm:mb-0">
-                <input
-                  type="text"
-                  name="full name"
-                  id="full name"
-                  placeholder="Full name"
-                  className="w-full px-4 py-2 text-white bg-black border border-white"
-                  disabled={!connected}
-                />
-              </div>
-              <div className="w-full px-2 sm:w-1/2">
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  placeholder="Email"
-                  className="w-full px-4 py-2 text-white bg-black border border-white"
-                  disabled={!connected}
-                />
-              </div>
-              <div className="w-full px-2 my-4 sm:w-1/2 sm:mb-0">
-                <input
-                  type="text"
-                  name="address"
-                  id="address"
-                  placeholder="Address"
-                  className="w-full px-4 py-2 text-white bg-black border border-white"
-                  disabled={!connected}
-                />
-              </div>
-              <div className="w-full px-2 sm:w-1/2 sm:mt-4">
-                <input
-                  type="text"
-                  name="city"
-                  id="city"
-                  placeholder="City"
-                  className="w-full px-4 py-2 text-white bg-black border border-white"
-                  disabled={!connected}
-                />
-              </div>
-              <div className="w-full px-2 my-4 sm:w-1/2 sm:mb-0">
-                <input
-                  type="text"
-                  name="postal"
-                  id="postal"
-                  placeholder="Postal"
-                  className="w-full px-4 py-2 text-white bg-black border border-white"
-                  disabled={!connected}
-                />
-              </div>
-              <div className="w-full px-2 sm:w-1/2 sm:mt-4">
-                <input
-                  type="text"
-                  name="country"
-                  id="country"
-                  placeholder="Country"
-                  className="w-full px-4 py-2 text-white bg-black border border-white"
-                  disabled={!connected}
-                />
+        <form onSubmit={handleSubmit}>
+          <div className="px-4 py-5 text-base sm:p-6">
+            <div className="flex flex-wrap -mx-2">
+              <div className="flex flex-col flex-wrap w-full -mx-2 sm:flex-row">
+                <div className="w-full px-2 mb-4 sm:w-1/2 sm:mb-0">
+                  <input
+                    type="text"
+                    name="fullName"
+                    id="full name"
+                    placeholder="Full name"
+                    className="w-full px-4 py-2 text-white bg-black border border-white"
+                    disabled={!connected}
+                    value={formData.fullName}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className="w-full px-2 sm:w-1/2">
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Email"
+                    className="w-full px-4 py-2 text-white bg-black border border-white"
+                    disabled={!connected}
+                    value={formData.email}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className="w-full px-2 my-4 sm:w-1/2 sm:mb-0">
+                  <input
+                    type="text"
+                    name="address"
+                    id="address"
+                    placeholder="Address"
+                    className="w-full px-4 py-2 text-white bg-black border border-white"
+                    disabled={!connected}
+                    value={formData.address}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className="w-full px-2 sm:w-1/2 sm:mt-4">
+                  <input
+                    type="text"
+                    name="city"
+                    id="city"
+                    placeholder="City"
+                    className="w-full px-4 py-2 text-white bg-black border border-white"
+                    disabled={!connected}
+                    value={formData.city}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className="w-full px-2 my-4 sm:w-1/2 sm:mb-0">
+                  <input
+                    type="text"
+                    name="postal"
+                    id="postal"
+                    placeholder="Postal"
+                    className="w-full px-4 py-2 text-white bg-black border border-white"
+                    disabled={!connected}
+                    value={formData.postal}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className="w-full px-2 sm:w-1/2 sm:mt-4">
+                  <input
+                    type="text"
+                    name="country"
+                    id="country"
+                    placeholder="Country"
+                    className="w-full px-4 py-2 text-white bg-black border border-white"
+                    disabled={!connected}
+                    value={formData.country}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className="w-full px-2 sm:w-1/4 sm:mt-4">
+                  <button
+                    type="submit"
+                    disabled={!connected}
+                    className="w-full px-4 py-2 text-white bg-black border border-white"
+                  >
+                    Submit
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
