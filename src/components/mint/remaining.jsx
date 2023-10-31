ec3bd6ec7dce745606deed3d449072f16f480c88
chixx <lucottejeremie@gmail.com>
2023-10-31 15:58:10 +0100

    uppgrade remaining infinite loop

--- src/components/mint/remaining.jsx
+++ src/components/mint/remaining.jsx
@@ -1,16 +1,20 @@
 import { useContractRead } from "wagmi";
 import ABI from "@/app/contract/abi/UNDOXXED.json";
+import { useEffect, useState } from "react";
+
+const maxSupply = 300;
+const maxSupplyToken1 = 150;
+const maxSupplyToken2 = 150;
 
 const Remaining = () => {
-  const maxSupply = 500;
-  const maxSupplyToken1 = 250;
-  const maxSupplyToken2 = 250;
+  const [token1Amount, setToken1Amount] = useState(0);
+  const [token2Amount, setToken2Amount] = useState(0);
+  const [totalAmount, setTotalAmount] = useState(0);
 
   const contractReadToken1 = useContractRead({
     address: "0x2d308a424474e2632a7cc10c9a6791f3f1b7192f",
     abi: ABI.abi,
     functionName: "getToken1Supply",
-    enabled: false,
     onError: (err) => {
       console.error(err);
     },
@@ -20,14 +24,11 @@ const Remaining = () => {
     address: "0x2d308a424474e2632a7cc10c9a6791f3f1b7192f",
     abi: ABI.abi,
     functionName: "getToken2Supply",
-    enabled: false,
     onError: (err) => {
       console.error(err);
     },
   });
 
-  const token1AmountMinted = Number(contractReadToken1.data);
-  const token2AmountMinted = Number(contractReadToken2.data);
   const getTotalAmountMinted = (value1, value2) => {
     if (typeof value1 === "number" && typeof value2 === "number") {
       return value1 + value2;
@@ -35,32 +36,39 @@ const Remaining = () => {
       return 0;
     }
   };
-  const totalAmountMinted = getTotalAmountMinted(
-    token1AmountMinted,
-    token2AmountMinted
-  );
+
+  useEffect(() => {
+    setToken1Amount(Number(contractReadToken1.data));
+  }, [contractReadToken1.data]);
+
+  useEffect(() => {
+    setToken2Amount(Number(contractReadToken2.data));
+  }, [contractReadToken2.data]);
+
+  useEffect(() => {
+    setTotalAmount(getTotalAmountMinted(token1Amount, token2Amount));
+  }, [token1Amount, token2Amount]);
 
   return (
     <div className="flex flex-col sm:flex-row sm:justify-between">
       <div>
         <p>Remaining</p>
         <p className="pt-1 text-base sm:text-2xl">
-          {totalAmountMinted ? maxSupply - totalAmountMinted : maxSupply}/
-          {maxSupply}
+          {totalAmount != 0 ? maxSupply - totalAmount : maxSupply}/{maxSupply}
         </p>
       </div>
       <div className="pt-4 sm:pt-0">
         <p>Remaining cover 1</p>
         <p className="text-base sm:pt-1 sm:text-2xl">
-          {token1AmountMinted ? token1AmountMinted : maxSupplyToken1}/
-          {maxSupplyToken1}
+          {token1Amount != 0 ? maxSupplyToken1 - token1Amount : maxSupplyToken1}
+          /{maxSupplyToken1}
         </p>
       </div>
       <div className="pt-4 sm:pt-0">
         <p>Remaining cover 2</p>
         <p className="pt-1 text-base sm:text-2xl">
-          {token2AmountMinted ? token2AmountMinted : maxSupplyToken2}/
-          {maxSupplyToken2}
+          {token2Amount != 0 ? maxSupplyToken2 - token2Amount : maxSupplyToken2}
+          /{maxSupplyToken2}
         </p>
       </div>
     </div>
