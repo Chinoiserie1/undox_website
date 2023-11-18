9b73491acc7d5a4225481ca7bc5d25d51860e653
chixx <lucottejeremie@gmail.com>
2023-11-18 13:59:08 +0100

    uppgrade text

--- src/components/details/aboutCard.js
+++ src/components/details/aboutCard.js
@@ -12,14 +12,32 @@ export default function AboutCard() {
         />
         <h1 className="pt-4 font-bold font-proxima_bold">About</h1>
         <p className="pt-4 font-light font-proxima_reg text-white/70">
-          We are thrilled to announce an exciting opportunity to showcase your
-          artistic skills and make a lasting impact on the upcoming book
-          “UNDOXXED” – a captivating exploration of “The Finest in Digital
-          Lifestyle Culture”. We are seeking a captivating and visually striking
-          design that encapsulates the essence of “UNDOXXED” and its exploration
-          of digital culture for the cover, spine, and back of the book. This is
-          an exceptional opportunity to leave your artistic imprint on a
-          significant publication that will resonate with enthusiasts worldwide.
+          &quot;UNDOXXED Book Vol. 1,&quot; is a the first book focuses on
+          &quot;The Finest in Digital Lifestyle Culture.&quot; and covers a wide
+          array of subjects, including fashion, streetwear, sneakers, street
+          art, design, and technology. Furthermore, it delves into the entire
+          digital fashion ecosystem where street culture & lifestyle merge to
+          Web3.0
+        </p>
+        <p className="pt-4 font-light font-proxima_reg text-white/70">
+          A critical aspect of this book is to emphasize the bridge between
+          In-Virtual-Life (IVL) and In-Real-Life (IRL) assets. This theme
+          &quot;phygital&quot; is prominently reflected in the editorial
+          content, subject selection, and the highlighting of physical elements.
+        </p>
+        <p className="pt-4 font-light font-proxima_reg text-white/70">
+          The approach of this book is purely editorial, and that there are no
+          advertising or payd content. The Undoxxed team acts as as
+          &quot;curator&quot;, seeking out brands, Web3 projects,
+          designer-creators, and artists who, in their judgment, are at the
+          forefront of the fashion and lifestyle ecosystem of the future. These
+          individuals and entities are chosen based on editorial criteria.
+        </p>
+        <p className="pt-4 font-light font-proxima_reg text-white/70">
+          The content featured in this book signifies a significant era and a
+          shift in how fashion is created, consumed, and experienced. It is a
+          responsibility to document and archive this transformation, which is
+          why &quot;UNDOXXED&quot; has come into existence.
         </p>
       </div>
     </div>
--- src/components/details/artistsCard.js
+++ src/components/details/artistsCard.js
@@ -4,9 +4,21 @@ export default function ArtistsCard() {
       <div className="w-full overflow-hidden border border-white shadow">
         <div className="px-4 py-5 sm:p-6">
           <h1 className="font-bold font-proxima_bold">Artists</h1>
+          <p className="pt-2 max-w-0font-bold font-proxima_bold">
+            The book features a diverse array of talents and entities,
+            including:
+          </p>
           <p className="pt-2 font-light font-proxima_reg text-white/70">
-            Dom Jordan, Dom Jordan, Dom Jordan, Dom Jordan, Dom Jordan, Dom
-            Jordan, Dom Jordan, Dom Jordan, Dom Jordan...
+            Adriana, Adriana Hoppenbrouwer, Agoria, Albert Lyp, Aliveform,
+            ALIVEFORM, Angelescu Studio, Antonio Talarico, Artisant, Ashumi
+            Sanghvi, Autrion, (B)APEVERSE, Big Xeus, Booyasan, Brand New Vision,
+            Buckets Club by Malbon Golf, Camille Louise Jewellery, Catty Tay,
+            Cult1vate, CryptoKaiju, Daniella Loftus, DMAT, DegenToonz, Dogami x
+            Gap, Dom Jordan, EFDOT, Eda Aguilar, Eyes Of Fashion, FacesOf Web3,
+            FabriX, Finn Rush Taylor, Flower Instincts By Sneakmart, Flowers For
+            Society, FoxyLab NYC, FuturFrontRow, Gmoney, Grace Robinson,
+            Grateful Ape Chocolate, HAPE, Hafid Reyes, Hermine Bourdin,
+            HexJerzo, Hublot x Takashi Murakami...
           </p>
         </div>
       </div>
