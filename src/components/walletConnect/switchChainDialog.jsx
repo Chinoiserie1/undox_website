c87cad0f99d6d09f445966aad7fb08b18e2c8575
chixx <lucottejeremie@gmail.com>
2023-11-27 11:24:28 +0100

    add env var for chain

--- src/components/walletConnect/switchChainDialog.jsx
+++ src/components/walletConnect/switchChainDialog.jsx
@@ -3,7 +3,7 @@ import { useAccount, useNetwork, useSwitchNetwork } from "wagmi";
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment } from "react";
 
-const networkId = 5;
+const networkId = process.env.NEXT_PUBLIC_CHAIN === 1 ? 1 : 5;
 
 const SwitchChainDialog = () => {
   const { address } = useAccount();
--- src/components/walletConnect/walletConnect.jsx
+++ src/components/walletConnect/walletConnect.jsx
@@ -12,7 +12,7 @@ import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
 // 1. Get projectId
 const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
 
-const chainUse = goerli;
+const chainUse = process.env.NEXT_PUBLIC_CHAIN === 1 ? mainnet : goerli;
 
 // 2. Create wagmiConfig
 const { chains, publicClient } = configureChains(
