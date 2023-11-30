184b11d470764d247532176039fbdb6280ac38bc
chixx <lucottejeremie@gmail.com>
2023-11-30 17:13:46 +0100

    uppgrade about card with bold begining each phrase

--- src/components/details/aboutCard.jsx
+++ src/components/details/aboutCard.jsx
@@ -4,55 +4,50 @@ export default function AboutCard() {
       <div className="w-full overflow-hidden border border-white shadow">
         <div className="px-4 py-5 sm:p-6">
           <h1 className="text-2xl font-bold text-purple-600">ABOUT</h1>
-          <ul className="ml-2 list-disc">
-            <li
-              className="pt-3 font-light text-white/70"
-              style={{ textAlign: "justify" }}
-            >
-              Type of Book: Hardcover coffee table book.
+          <ul className="ml-2 list-disc font-ligth">
+            <li className="pt-3 text-white/70" style={{ textAlign: "justify" }}>
+              <span className="font-semibold text-white/90">Type of Book:</span>{" "}
+              Hardcover coffee table book.
             </li>
-            <li
-              className="pt-3 font-light text-white/70"
-              style={{ textAlign: "justify" }}
-            >
-              Number of Pages: 204 pages.
+            <li className="pt-3 text-white/70" style={{ textAlign: "justify" }}>
+              <span className="font-semibold text-white/90">
+                Number of Pages:
+              </span>{" "}
+              204 pages.
             </li>
-            <li
-              className="pt-3 font-light text-white/70"
-              style={{ textAlign: "justify" }}
-            >
-              Available Colors: Black or Purple, both containing the same
-              content.
+            <li className="pt-3 text-white/70" style={{ textAlign: "justify" }}>
+              <span className="font-semibold text-white/90">
+                Available Colors:
+              </span>{" "}
+              Black or Purple, both containing the same content.
             </li>
-            <li
-              className="pt-3 font-light text-white/70"
-              style={{ textAlign: "justify" }}
-            >
-              Limited Edition: 200 NFT-Book copies.
+            <li className="pt-3 text-white/70" style={{ textAlign: "justify" }}>
+              <span className="font-semibold text-white/90">
+                Limited Edition:
+              </span>{" "}
+              200 NFT-Book copies.
             </li>
-            <li
-              className="pt-3 font-light text-white/70"
-              style={{ textAlign: "justify" }}
-            >
-              Special NFT Offer: Each NFT comes with a physical book.
+            <li className="pt-3 text-white/70" style={{ textAlign: "justify" }}>
+              <span className="font-semibold text-white/90">
+                Special NFT Offer:
+              </span>{" "}
+              Each NFT comes with a physical book.
             </li>
-            <li
-              className="pt-3 font-light text-white/70"
-              style={{ textAlign: "justify" }}
-            >
-              Integrated Technology: NFC chip embedded in the back cover.
+            <li className="pt-3 text-white/70" style={{ textAlign: "justify" }}>
+              <span className="font-semibold text-white/90">
+                Integrated Technology:
+              </span>{" "}
+              NFC chip embedded in the back cover.
             </li>
-            <li
-              className="pt-3 font-light text-white/70"
-              style={{ textAlign: "justify" }}
-            >
-              Limited Book Edition: Maximum of 500 copies in total.
+            <li className="pt-3 text-white/70" style={{ textAlign: "justify" }}>
+              <span className="font-semibold text-white/90">
+                Limited Book Edition:
+              </span>{" "}
+              Maximum of 500 copies in total.
             </li>
-            <li
-              className="pt-3 font-light text-white/70"
-              style={{ textAlign: "justify" }}
-            >
-              Sales Method: Offered on a first-come, first-served basis.
+            <li className="pt-3 text-white/70" style={{ textAlign: "justify" }}>
+              <span className="font-semibold text-white/90">Sales Method:</span>{" "}
+              Offered on a first-come, first-served basis.
             </li>
           </ul>
         </div>
--- src/components/details/details.jsx
+++ src/components/details/details.jsx
@@ -10,7 +10,7 @@ export default function Details() {
       <h1 className="font-bold font-[TTMoons]">DETAILS</h1>
       <div className="pt-10" />
       <PhasePricingCard />
-      <div className="flex flex-col pt-10 sm:flex-row">
+      <div className="flex flex-col pt-10 text-sm sm:flex-row lg:text-xl">
         <UndoxxedBookVol1Card />
         <div className="flex flex-col pt-4 sm:w-1/2 sm:pt-0 sm:pl-10">
           {/* <div className="w-full overflow-hidden border border-white shadow"> */}
