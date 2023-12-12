26afbaf00eb6504a466f52382e65168762c6a6ea
chixx <lucottejeremie@gmail.com>
2023-12-12 16:13:06 +0100

    uppgrade home page preview

--- src/components/preview/preview.jsx
+++ src/components/preview/preview.jsx
@@ -4,8 +4,8 @@ export default function Preview() {
   return (
     <>
       <div className="text-center">
-        <h1 className="inline-block px-10 py-5 text-3xl font-bold text-white uppercase border-white md:text-4xl border-3">
-          Preview
+        <h1 className="inline-block px-10 py-5 text-3xl font-bold text-white uppercase md:text-4xl">
+          book Preview
         </h1>
       </div>
 
