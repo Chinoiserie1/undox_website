a59cd7fb2cfe4ef9948f946993eb6650edab1513
chixx <lucottejeremie@gmail.com>
2024-01-10 21:02:17 +0100

    add chat on mint page

--- src/pages/mint.jsx
+++ src/pages/mint.jsx
@@ -6,16 +6,22 @@ import ClientOnly from "@/components/walletConnect/clientOnly";
 
 export default function MintPage() {
   return (
-    <div className="text-white bg-ob-blackbg">
-      <div className="px-6 mx-auto mb-10 text-xs max-w-7xl md:px-20 lg:px-32 md:text-base lg:text-lg">
-        <ClientOnly>
-          <MintLayout>
-            <WalletConnectDialog />
-            <SwitchChainDialog />
-            <Mint />
-          </MintLayout>
-        </ClientOnly>
+    <>
+      <div className="text-white bg-ob-blackbg">
+        <div className="px-6 mx-auto mb-10 text-xs max-w-7xl md:px-20 lg:px-32 md:text-base lg:text-lg">
+          <ClientOnly>
+            <MintLayout>
+              <WalletConnectDialog />
+              <SwitchChainDialog />
+              <Mint />
+            </MintLayout>
+          </ClientOnly>
+        </div>
       </div>
-    </div>
+      <script
+        src="https://embed.small.chat/T06DC8L1BBNC06DC9KL9LL.js"
+        async
+      ></script>
+    </>
   );
 }
